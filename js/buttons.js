03b5f3053a5219aee661ecb097a066d6b6562e7f
abduze <abduze@gmail.com>
2016-11-04 11:28:38 +0800

    Update buttons.js

--- js/buttons.js
+++ js/buttons.js
@@ -15,7 +15,7 @@
     along with Tianjin Mahjong. If not, see <http://www.gnu.org/licenses/>.
 */
 
-// 清除所有按钮d
+// 清除所有按钮
 var clearButtons=function () {
   var x=$i("xzm");
   x.style.display="none";
